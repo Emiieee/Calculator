cf05cd7e235fdaa2c476b042795b138c506c41d3
emiieee <embarri3@gmail.com>
2024-07-17 15:43:48 -0500

    Added the remaining math functions. Modified main function to a do while loop.

--- main.cpp
+++ main.cpp
@@ -17,8 +17,10 @@ void subtraction();
 void multiplication();
 void division();
 
+//Validation Prototypes
 int validate_intRange(int userInput, int range_min, int range_max);
 double validate_double(double userInput);
+
 
 /********************************************************************************************************************
 *																													*
@@ -27,43 +29,54 @@ double validate_double(double userInput);
 ********************************************************************************************************************/
 void main()
 {
+	//Variables
+	bool endProgram = false;
 	//MENU
-	int userInput_menu;	//holds user input for selecting from menu options.
-	cout << std::string(100, '-') << endl;	//string(#,C) C is outputted # amount of times.
-	cout << "SELECT A FUNCTION FROM BELOW:" << endl
-		<< "1. Addition" << endl
-		<< "2. Subtraction" << endl
-		<< "3. Multiplication" << endl
-		<< "4. Division" << endl;
-	cout << std::string(100, '-') << endl;
-	//Get user input
-	cout << "SELECTION: ";
-	cin >> userInput_menu;
-	//INPUT VALIDATION - user input with a range between 0 and 4
-	userInput_menu = validate_intRange(userInput_menu, 0, 4);
-	//
-	cout << "User selected: " << userInput_menu << endl;
-
-	//Switch statement for menu
-	switch (userInput_menu)
+	do
 	{
-	case 1:
-		cout << "\nAddition chosen.\n";
-		addition();
-		break;
-	case 2:
-		cout << "\nSubtraction chosen.\n";
-		subtraction();
-		break;
-	case 3:
-		cout << "\nMultiplication chosen.\n";
-		multiplication();
-		break;
-	case 4:
-		cout << "\nDivision chosen.\n";
-		division();
-		break;
+		int userInput_menu;	//holds user input for selecting from menu options.
+		cout << std::string(100, '-') << endl;	//string(#,C) C is outputted # amount of times.
+		cout << "SELECT A FUNCTION FROM BELOW:" << endl
+			<< "1. Addition" << endl
+			<< "2. Subtraction" << endl
+			<< "3. Multiplication" << endl
+			<< "4. Division" << endl
+			<< "5. Exit Program" << endl;
+		cout << std::string(100, '-') << endl;
+		//Get user input
+		cout << "SELECTION: ";
+		cin >> userInput_menu;
+		//INPUT VALIDATION - user input with a range between 0 and 5
+		userInput_menu = validate_intRange(userInput_menu, 0, 5);
+		//
+		cout << "User selected: " << userInput_menu << endl;
+
+		//Switch statement for menu
+		switch (userInput_menu)
+		{
+		case 1:
+			cout << "\nAddition chosen.\n";
+			addition();
+			break;
+		case 2:
+			cout << "\nSubtraction chosen.\n";
+			subtraction();
+			break;
+		case 3:
+			cout << "\nMultiplication chosen.\n";
+			multiplication();
+			break;
+		case 4:
+			cout << "\nDivision chosen.\n";
+			division();
+			break;
+		case 5:
+			cout << "\nEnding program.\n";
+			endProgram = true;
+		};
 	}
+	while (endProgram != true);
+	
 
 
 
@@ -78,7 +91,7 @@ void addition()
 	double varB;
 	double solve = 0;	//Holds the solution.
 
-	//Get user input
+	//Get user input - addition
 	cout << "Enter the first number: ";
 	cin >> varA;
 	//Input validation
@@ -93,18 +106,79 @@ void addition()
 	//Output the answer.
 	cout << varA << " + " << varB << " = " << solve << endl;
 
-	return;	//Return the solution.
+	//End of function
+	return;
 };
 void subtraction()
 {
+	//variables
+	double varA;	//Number subtracted from
+	double varB;	//Subtracting number
+	double solve;	//Holds solution
+
+	//Get user input - subtraction
+	cout << "Enter the first number: ";
+	cin >> varA;
+	varA = validate_double(varA);	//Input validation
+	cout << "Enter the second number: ";
+	cin >> varB;
+	varB = validate_double(varB);	//Input validation
+
+	//calculate result
+	solve = varA - varB;
+
+	//Print result
+	cout << varA << " - " << varB << " = " << solve << endl;
+
+	//End of function
 	return;
 };
 void multiplication()
 {
+	//Variables
+	double varA;
+	double varB;
+	double solve;
+
+	//Get input
+	cout << "Enter the first number: ";
+	cin >> varA;
+	varA = validate_double(varA);	//input validation
+	cout << "Enter the second number: ";
+	cin >> varB;
+	varB = validate_double(varB);	//input validation
+	
+	//calculate
+	solve = varA * varB;
+
+	//output
+	cout << varA << " * " << varB << " = " << solve << endl;
+	
+	//End of function
 	return;
 };
 void division()
 {
+	//variables
+	double varA;
+	double varB;
+	double solve;
+
+	//Get input
+	cout << "Enter the first number: ";
+	cin >> varA;
+	varA = validate_double(varA);	// input validation
+	cout << "Enter the second number: ";
+	cin >> varB;
+	varB = validate_double(varB);	// input validation
+
+	//calculate
+	solve = varA / varB;
+
+	//output
+	cout << varA << " / " << varB << " = " << solve;
+
+	//End of function
 	return;
 };
 
@@ -113,11 +187,14 @@ int validate_intRange(int userInput, int range_min, int range_max)
 {
 	while (cin.fail() || userInput < range_min || userInput > range_max)
 	{
+		//Clear the error and ignore the input.
 		cin.clear();
 		cin.ignore();
+		//Prompt user to input a valid number
 		cout << "The number you entered is an invalid option. Reenter your selection : ";
 		cin >> userInput;
 	};
+	//Return the corrected input
 	return userInput;
 };
 
@@ -125,10 +202,13 @@ double validate_double(double userInput)
 {
 	while (cin.fail())
 	{
+		//Clear the error and ignore the input.
 		cin.clear();
 		cin.ignore();
+		//Prompt user to input a valid number
 		cout << "The number previously entered is an invalid option. Re-enter your selection: ";
 		cin >> userInput;
 	}
+	//Return the corrected input
 	return userInput;
 }
